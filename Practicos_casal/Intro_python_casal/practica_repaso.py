cec86a916a026a290ec8ef43c17ba60d6e5ee55d
MerCasal03 <mechicasal@gmail.com>
2022-04-18 21:45:31 -0300

    Actualizo

---
+++ Practicos_casal/Intro_python_casal/practica_repaso.py
@@ -0,0 +1,26 @@
+#1
+potencias={}
+for i in range(1, 15):
+    valor = i
+    potencia= i ** 2
+    potencias[i]=potencia
+
+print(potencias)
+
+#2
+numeros={"valor_1": 1, "valor_2":2, "valor_3":3}
+total=0
+for i in numeros.values():
+    total += i
+print(total)
+
+#3
+lista_strings=["hola", "a la grnade le puse cuca", "hasta pronto"]
+longitud_strings=[]
+for i in lista_strings:
+    largo=len[lista_strings[i]]
+    longitud_strings.append(largo)
+print(longitud_strings)
+
+#4
+
